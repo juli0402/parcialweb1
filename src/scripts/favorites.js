b5371c52caeeb16203dc17733eea958715b6e565
juli0402 <mj.rojass1@uniandes.edu.co>
2021-03-15 20:26:55 -0500

    parcialweb1

--- src/scripts/favorites.js
+++ src/scripts/favorites.js
@@ -1,6 +1,7 @@
 let favorites = localStorage.getItem("favoritos");
 const productsHtml = document.querySelector(".products");
 let html = String(document.getElementById("divFavoritos"));
+
 if (favorites) {
   favorites = favorites.split(";");
   favorites.forEach((favorite) => {
@@ -9,16 +10,16 @@ if (favorites) {
     const singleProductHtml = document.createElement("div");
     singleProductHtml.className = "row product";
     singleProductHtml.innerHTML = `
-                        <div class="col-3">
+                        <div class="col-3" style = "margin-bottom: auto; margin-top: auto;">
                             <a href="productDetail.html?id=${
                               element.id
-                            }" style="display: flex;align-items: center;flex-wrap: wrap;">
+                            }" style="display: flex;align-items: center;flex-wrap: wrap; margin-bottom: auto; margin-top: auto;">
                                 <img src="${
                                   element.picture
                                 }" class="img-fluid mx-auto d-block" alt="" style="width: 90px; height: 90px;">
                             </a>
                         </div>
-                        <div class="col-6">
+                        <div class="col-6" style = "margin-bottom: auto; margin-top: auto;">
                             <p>$${element.price.amount}
                                 <img src="./src/img/free-delivery.png" class="${
                                   element.free_shipping ? "" : "d-none"
@@ -26,9 +27,16 @@ if (favorites) {
                             </p>
                             <p>${element.title}</p>
                         </div>
-                        <div class="col-3">
-                            <p>${element.location}</p>
+                        <div class="col-3" style="margin-top: auto; margin-bottom: auto;">
+                            <p>
+                                <a href="productDetail.html?id=${
+                                  element.id
+                                }" style="display: flex;align-items: center;flex-wrap: wrap; text-decoration: none;">
+                                    <button style="background: #C968CB; border-radius: 20px; color: white; display: block; margin: 5px; width: 100%;height: 51px;" class="btn" >Ver Articulo</button>
+                                </a>
+                            </p>
                         </div>
+
                 `;
 
     productsHtml.appendChild(singleProductHtml);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,13 @@
-data.items.forEach((element) => {
-  const productsHtml = document.querySelector(".products");
+let categorySearch = document.getElementById("search");
+let button = document.getElementById("button");
+const productsHtml = document.querySelector(".products");
+let singleProductHtml;
 
-  const singleProductHtml = document.createElement("div");
+data.items.forEach((element) => {
+  singleProductHtml = document.createElement("div");
   singleProductHtml.className = "row product";
   singleProductHtml.innerHTML = `
-                <div class="col-3">
+                <div class="col-3" style = "margin-bottom: auto; margin-top: auto;">
                     <a href="productDetail.html?id=${
                       element.id
                     }" style="display: flex;align-items: center;flex-wrap: wrap;">
@@ -13,7 +16,7 @@ data.items.forEach((element) => {
                         }" class="img-fluid mx-auto d-block" alt="" style="width: 90px; height: 90px;">
                     </a>
                 </div>
-                <div class="col-6">
+                <div class="col-6" style = "margin-bottom: auto; margin-top: auto;">
                     <p>$${element.price.amount}
                         <img src="./src/img/free-delivery.png" class="${
                           element.free_shipping ? "" : "d-none"
@@ -27,4 +30,58 @@ data.items.forEach((element) => {
         `;
 
   productsHtml.appendChild(singleProductHtml);
+});
+
+button.onclick = handleSearch = () => {
+  if (categorySearch.value !== "") {
+    productsHtml.innerHTML = ``;
+    let encontro = false;
+    data.items.forEach((element) => {
+      element.categories.forEach((category) => {
+        if (category.toUpperCase() == categorySearch.value.toUpperCase()) {
+          encontro = true;
+          singleProductHtml = document.createElement("div");
+          singleProductHtml.className = "row product";
+          singleProductHtml.innerHTML = `
+                    <div class="col-3" style = "margin-bottom: auto; margin-top: auto;">
+                        <a href="productDetail.html?id=${
+                          element.id
+                        }" style="display: flex;align-items: center;flex-wrap: wrap;">
+                            <img src="${
+                              element.picture
+                            }" class="img-fluid mx-auto d-block" alt="" style="width: 90px; height: 90px;">
+                        </a>
+                    </div>
+                    <div class="col-6" style = "margin-bottom: auto; margin-top: auto;">
+                        <p>$${element.price.amount}
+                            <img src="./src/img/free-delivery.png" class="${
+                              element.free_shipping ? "" : "d-none"
+                            }" style="width: 25px;" alt="">
+                        </p>
+                        <p>${element.title}</p>
+                    </div>
+                    <div class="col-3">
+                        <p>${element.location}</p>
+                    </div>
+            `;
+          productsHtml.appendChild(singleProductHtml);
+        }
+      });
+    });
+    if (encontro == false) {
+      productsHtml.innerHTML = `<div class="alert alert-warning" role="alert">
+    No existe esta categoría
+    </div>`;
+    }
+  } else {
+    productsHtml.innerHTML = `<div class="alert alert-warning" role="alert">
+    Inserte la categoría que desea buscar
+    </div>`;
+  }
+};
+
+document.getElementById("form").addEventListener("submit", function (e) {
+  handleSearch();
+  e.preventDefault();
+  usrpas();
 });
